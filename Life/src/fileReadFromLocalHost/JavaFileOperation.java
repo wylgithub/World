25f5f4fb65c79b16e4030d6d3697673f76eed9cd
Administrator <Administrator@PC-20150112PHQA>
2016-03-16 20:50:55 +0800

    编写java对文件操作的函数

---
+++ Life/src/fileReadFromLocalHost/JavaFileOperation.java
@@ -0,0 +1,111 @@
+package fileReadFromLocalHost;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * 这个类用于使用java读取或者或者写入内容到本地磁盘中指定的文件
+ * @author Administrator
+ *
+ */
+public class JavaFileOperation {
+
+	/**
+	 * 读取本地磁盘路径的文件显示在终端上
+	 * @param filePath： 读取文件的本地路径 
+	 */
+	public static void ReadFileFromLocalHost(final String filePath){
+		try{
+			//读取本地文件
+			FileInputStream file = new FileInputStream(filePath);
+			//把字符输入流转换成字节流,套上缓冲的管子(InputStreamReader， 它是字节转换为字符的桥梁)
+			BufferedReader buf = new BufferedReader(new InputStreamReader(file));
+			
+			String str = null;
+			//记录文件的读取行内容
+			int line = 1;
+			//一行一行的读取文件中的内容
+			while((str = buf.readLine()) != null){
+				System.out.println("第"+line+"行的内容为: "+str);
+				line++;
+			}
+			//关闭文件
+			file.close();
+		}catch(IOException ex){
+			System.out.println("文件读取失败!");
+			ex.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 在本地磁盘的指定目录创建指定的文件
+	 * @param filePath 文件所在的路径
+	 * @param fileName 文件名+后缀名
+	 */
+	public static void createFileToLocalHost(final String filePath,final String fileName){
+		//创建一个文件创建对象，在指定的磁盘目录下
+		File file = new File(filePath,fileName);
+		try{
+
+			//创建文件
+			file.createNewFile();
+			System.out.println("文件创建成功！");
+		}catch(IOException ex){
+			System.out.println("文件创建失败！");
+			//打印异常堆栈
+			ex.printStackTrace();
+		}
+	}
+	
+	//将缓冲区的内容输入到本地磁盘中的文件
+	public static void WriteFileToLocalHost(final String filePath){
+		
+		//输入字符
+		Scanner console = new Scanner(System.in);
+		System.out.println("请输入内容: ");
+		
+//		int i=1;
+//		while(i<1000000){
+//			System.out.println("输入完成,正在保存.");
+//			System.out.println("输入完成,正在保存..");
+//			System.out.println("输入完成,正在保存...");
+//			System.out.println("输入完成,正在保存....");
+//			System.out.println("输入完成,正在保存.....");
+//			System.out.println("输入完成,正在保存......");
+//			System.out.println("输入完成,正在保存.......");
+//			System.out.println("输入完成,正在保存........");
+//			System.out.println("输入完成,正在保存..........");
+//			i++;
+//		}
+		//输入字符
+		String str = console.next();
+		byte bt[] = new byte[10240];
+		bt = str.getBytes();
+		try{
+			//实例化一个文件写入对象
+			FileOutputStream in = new FileOutputStream(filePath);
+			try{
+				//写入文件
+				in.write(bt, 0, bt.length);
+				System.out.println("输入完成,正在保存......");	
+				System.out.println("文件写入成功！");
+			}catch(Exception ex){
+				System.out.println("文件写入异常！");
+				ex.printStackTrace();
+			}finally{
+				//关闭文件写入
+				in.close();
+			}
+		}catch(IOException ex){	
+			System.out.println("文件写入失败!");
+			//打印异常堆栈
+			ex.printStackTrace();
+		}
+		
+	}
+}
---
+++ Life/src/fileReadFromLocalHost/ReadFileFromLocalHost.java
@@ -0,0 +1,49 @@
+package fileReadFromLocalHost;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReadFileFromLocalHost {
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		
+//		String Url = "D:/readfile.txt";
+//
+//		try {
+//			//E:/Java/fileReadTest/readfile.txt"
+//			FileInputStream file = new FileInputStream(Url);
+//			//把字符输入流转换成字节流，并且套上缓冲流管子
+//			BufferedReader buf = new BufferedReader(new InputStreamReader(file));
+//			String str = null;
+//			int i = 1;
+//			while((str = buf.readLine()) != null){
+//				System.out.println("第"+i+"行:"+str);
+//				i++;
+//			}
+//		} catch (FileNotFoundException e) {
+//			System.out.println("文件读取异常！请检查！");
+//			e.printStackTrace();
+//		}
+//		
+//	}
+//	String path = "E:/Java/fileReadTest/readfile.txt";
+	JavaFileOperation file =new JavaFileOperation();
+//	file.ReadFileFromLocalHost(path);
+	
+	//创建一个文件,test.txt
+	String filePath = "E:/Java/fileReadTest";
+	String fileName = "test.txt";
+	String fullFilePath = "E:/Java/fileReadTest/test.txt";
+	//创建文件
+	//file.createFileToLocalHost(filePath, fileName);
+	//向文件内写入内容
+	file.WriteFileToLocalHost(fullFilePath);
+	}
+}
